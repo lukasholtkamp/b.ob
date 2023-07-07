a8fe6eef2e53a52ec642ed787ceabba829517413
yigit-hshl <56076926+yigit-hshl@users.noreply.github.com>
2023-07-07 14:04:36 +0200

    fix: resolve conflicts

--- src/catkin_ws/src/motor_driver_library/include/motor_driver_library/RunMotor.h
+++ src/catkin_ws/src/motor_driver_library/include/motor_driver_library/RunMotor.h
@@ -1,15 +1,15 @@
 #ifndef RUN_MOTOR_H
 #define RUN_MOTOR_H
 
-#include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
-#include <sensor_msgs/Joy.h>
-#include <ros/console.h>
+#include <ros/ros.h> //<-- Used for the ROS environment
+#include <geometry_msgs/Twist.h> //<-- Used to receive messages from the cmd_vel topic
+#include <sensor_msgs/Joy.h> //<-- Used to receive messages from the joy topic
+#include <ros/console.h> //<-- Used to change the verbosity of the program
 
-#define MIN_SPEED               0.0
-#define MAX_SPEED               100.0
-#define VELOCITY_MULTIPLIER     100.0
-#define FREQUENCY               1000
+#define MIN_SPEED               0.0 //<-- The minimum speed of the robot
+#define MAX_SPEED               100.0 //<-- The maximum speed of the robot
+#define VELOCITY_MULTIPLIER     100.0 //<-- The multiplier used to convert the linear and angular velocities to a percentage
+#define FREQUENCY               1000 //<-- The frequency of the PWM signal
 
 
 #endif // RUN_MOTOR_H
