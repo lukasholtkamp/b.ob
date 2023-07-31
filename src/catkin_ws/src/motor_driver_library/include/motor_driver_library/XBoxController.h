c1da2e60101ae6823bed9cd242514d485d1ff9a1
JanHopf <50075181+JanHopf@users.noreply.github.com>
2023-07-31 09:22:54 +0200

    Cleaned up XBoxController.h

--- src/catkin_ws/src/motor_driver_library/include/motor_driver_library/XBoxController.h
+++ src/catkin_ws/src/motor_driver_library/include/motor_driver_library/XBoxController.h
@@ -1,27 +1,11 @@
 #ifndef X_BOX_CONTROLLER_H
 #define X_BOX_CONTROLLER_H
 
-<<<<<<< HEAD
   #include <ros/ros.h> //<-- Used for the ROS environment
   #include <geometry_msgs/Twist.h> //<-- Used to receive messages from the cmd_vel topic
   #include <sensor_msgs/Joy.h> //<-- Used to receive messages from the joy topic
   #include <ostream> //<-- Used to print to the console
-=======
-#include <ros/ros.h> //<-- Used for the ROS environment
-#include <geometry_msgs/Twist.h> //<-- Used to receive messages from the cmd_vel topic
-#include <sensor_msgs/Joy.h> //<-- Used to receive messages from the joy topic
-#include <ostream> //<-- Used to print to the console
-#include <stdlib.h>
 
-//--Button configuration according to XBoxOne Controller
-#define A_BUTTON 0 
-#define B_BUTTON 1 
-#define Y_BUTTON 4
-//--Axes configuration according to XBoxOne Controller
-#define ANGULAR_VEL 0 
-#define JOY_AXIS_RT 4
-#define JOY_AXIS_LT 5
->>>>>>> e2e3b9bb5f8f35e25d20103397357cec4b69d929
 
   //--Button configuration according to XBoxOne Controller
   #define A_BUTTON 0
