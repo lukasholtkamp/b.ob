305d97c1114ef46f2f01d1f1e87ed5bce6538f96
horstmal <luca.horstmann@bertrandt.com>
2024-04-17 11:31:21 +0200

    initial code

---
+++ src/joystick/launch/joystick.launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from eingabe import joy, joy_a
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    if (joy(3, 0) == true):
+
+        print('Key X, Basic Drive Mode enabled!')
+
+        joy_params = os.path.join(get_package_share_directory('src'),'config','joystick,yaml')
+
+        joy_node = Node(
+
+            package = 'joy',
+            executable = 'joy_node',
+            parameters = [joy_params],
+        )
+
+        teleop_node = Node(
+
+            package = 'teleop_twist_joy',
+            executable = 'teleop_node',
+            name = 'teleop_node'
+            parameters = [joy_params],
+        )
+
+        return LaunchDescription([
+
+            joy_node,
+            teleop_node,
+        ])
