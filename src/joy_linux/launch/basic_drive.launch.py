0dde711f8dc3b490fa509a38881f46590678b4ac
horstmal <luca.horstmann@bertrandt.com>
2024-05-07 14:42:04 +0200

    Update code with formatter

--- src/joy_linux/launch/basic_drive.launch.py
+++ src/joy_linux/launch/basic_drive.launch.py
@@ -9,24 +9,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    # use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # set the Parameters from BasicDrive.yaml as joy_params
     joy_params = os.path.join(Path.cwd(), "config", "BasicDrive.yaml")
 
-    # starting the joy node
-    joy_linux_node = Node(
-        package="joy_linux",
-        executable="joy_linux_node",
-    )
-
-    # starting the node for the drive modes
-    sub_node = Node(
-        package="joy_linux",
-        executable="sub_node",
-    )
-
-    # starting the teleop node with parameters from BasicDrive.yaml
     teleop_node = Node(
         package="teleop_twist_joy",
         executable="teleop_node",
@@ -34,22 +19,12 @@ def generate_launch_description():
         parameters=[joy_params],
     )
 
-    # twist_stamper = Node(
-    #         package='twist_stamper',
-    #         executable='twist_stamper',
-    #         parameters=[{'use_sim_time': use_sim_time}],
-    #         remappings=[('/cmd_vel_in','/diff_cont/cmd_vel_unstamped'),
-    #                     ('/cmd_vel_out','/diff_cont/cmd_vel')]
-    #      )
-
     return LaunchDescription(
         [
             # DeclareLaunchArgument(
             #   'use_sim_time',
             #    default_value='false',
             #     description='Use sim time if true'),
-            joy_linux_node,
-            sub_node,
             teleop_node,
             # twist_stamper
         ]
--- src/joy_linux/src/sub_node.cpp
+++ src/joy_linux/src/sub_node.cpp
@@ -10,77 +10,88 @@
 
 using std::placeholders::_1;
 
-std::string find_button(std::vector<int> buttons,int size);
+std::string find_button(std::vector<int> buttons, int size);
+// std::string find_axes(std::vector<float> axes,float size);
 
 class DriveModeSubscriber : public rclcpp::Node
 {
-    public:
+public:
     DriveModeSubscriber()
-    : Node("drive_mode")
+        : Node("drive_mode")
     {
-    subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
-    "joy", 10, std::bind(&DriveModeSubscriber::topic_callback, this, _1));
+        subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
+            "joy", 10, std::bind(&DriveModeSubscriber::topic_callback, this, _1));
     }
 
 private:
-    void topic_callback(const sensor_msgs::msg::Joy & msg) const
-        {
-        std::string button=find_button(msg.buttons,msg.buttons.size());
+    void topic_callback(const sensor_msgs::msg::Joy &msg) const
+    {
+        std::string button = find_button(msg.buttons, msg.buttons.size());
+        // std::string axes=find_axes(msg.axes,msg.axes.size());
 
-        if(button!=""){
-            std::cout << button << std::endl ;
+        if (button != "")
+        {
+            std::cout << button << std::endl;
             std::this_thread::sleep_for(std::chrono::milliseconds(1));
             system("clear");
-            std::cout << button << std::endl ;
+            std::cout << button << std::endl;
         }
-        
-        }
+
+        // if(axes!=""){
+        //     std::cout << axes << std::endl;
+        //     std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        //     system("clear");
+        //     std::cout << axes << std::endl ;
+        // }
+    }
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
 };
 
-std::string find_button(std::vector<int> buttons,int size){
-    int pressed=-1;
+std::string find_button(std::vector<int> buttons, int size)
+{
+    int pressed = -1;
 
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++)
+    {
 
-        if(buttons[i]>0)
+        if (buttons[i] > 0)
         {
-            pressed=i;
+            pressed = i;
             break;
         }
     }
 
-        if (pressed==0)
-        {
-            //Button A
-            return "Assisted Drive Mode Enabled";
-        }
-        if (pressed==1)
-        {
-            //Button B
-            return "Emergency Stop";
-        }
-        if (pressed==4)
-        {
-            //Button Y
-            return "";
-        }
-        if (pressed==3)
-        {
-            //Button X
-            return "Basic Drive Mode Enabled";
-        }
-        else
-        {
-            return "";
-        }
-
+    if (pressed == 0)
+    {
+        // Button A
+        return "Assisted Drive Mode Enabled";
+    }
+    if (pressed == 1)
+    {
+        // Button B
+        return "Emergency Stop";
+    }
+    if (pressed == 4)
+    {
+        // Button Y
+        return "";
+    }
+    if (pressed == 3)
+    {
+        // Button X
+        // system("ros2 launch basic_drive.launch.py");
+        return "Basic Drive Mode Enabled";
+    }
+    else
+    {
+        return "";
+    }
 }
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-rclcpp::init(argc, argv);
-rclcpp::spin(std::make_shared<DriveModeSubscriber>());
-rclcpp::shutdown();
-return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<DriveModeSubscriber>());
+    rclcpp::shutdown();
+    return 0;
 }
