b7a14db438cc68d9811661584845895f48e665b1
jkan67-de <jkan67.de@gmail.com>
2024-05-14 14:54:15 +0200

    removed nodes from launch

--- src/diffdrive_bob/bringup/launch/diffbot.launch.py
+++ src/diffdrive_bob/bringup/launch/diffbot.launch.py
@@ -18,17 +18,12 @@ from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
 
-import launch
-import launch_ros.actions
-
 from pathlib import Path
 import os
 import yaml
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
-from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
@@ -135,34 +130,6 @@ def generate_launch_description():
             on_exit=[robot_controller_spawner],
         )
     )
-
-    joy_config = launch.substitutions.LaunchConfiguration('joy_config')
-    joy_dev = launch.substitutions.LaunchConfiguration('joy_dev')
-    publish_stamped_twist = launch.substitutions.LaunchConfiguration('publish_stamped_twist')
-    config_filepath = launch.substitutions.LaunchConfiguration('config_filepath')
-
-    launch.actions.DeclareLaunchArgument('joy_vel', default_value='cmd_vel'),
-    launch.actions.DeclareLaunchArgument('joy_config', default_value='xbox'),
-    launch.actions.DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0'),
-    launch.actions.DeclareLaunchArgument('publish_stamped_twist', default_value='true'),
-    launch.actions.DeclareLaunchArgument('config_filepath', default_value=[
-        launch.substitutions.TextSubstitution(text=os.path.join(
-            get_package_share_directory('teleop_twist_joy'), 'config', '')),
-        joy_config, launch.substitutions.TextSubstitution(text='.config.yaml')]),
-
-    launch_ros.actions.Node(
-        package='joy', executable='joy_node', name='joy_node',
-        parameters=[{
-            'dev': joy_dev,
-            'deadzone': 0.3,
-            'autorepeat_rate': 20.0,
-        }]),
-    launch_ros.actions.Node(
-        package='teleop_twist_joy', executable='teleop_node',
-        name='teleop_twist_joy_node',
-        parameters=[config_filepath, {'publish_stamped_twist': publish_stamped_twist}],
-        remappings=[('/cmd_vel', '/diffbot_base_controller/cmd_vel')],
-        )                               
 
     nodes = [
         control_node,
