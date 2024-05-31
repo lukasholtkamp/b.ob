534a0a98caa417fe53cce42793d1593643698875
jkan67-de <jkan67.de@gmail.com>
2024-05-31 21:21:38 +0200

    added saved map and localisation script

---
+++ src/bob_navigation/launch/sim_localisation.launch.py
@@ -0,0 +1,76 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+
+from pathlib import Path
+
+
+def generate_launch_description():
+
+    pkg_slam = os.path.join(Path.cwd(), "src", "bob_slam")
+    pkg_sim = os.path.join(Path.cwd(), "src", "bob_simulation")
+    pkg_navigation = os.path.join(Path.cwd(),"src","bob_navigation")
+
+    localisation_params_file = os.path.join(pkg_navigation,"config","mapper_params_localisation.yaml")
+
+    # Launch configuration variables specific to simulation
+    rviz = LaunchConfiguration("rviz")
+    slam_params_file = LaunchConfiguration('slam_params_file')
+
+    declare_rviz = DeclareLaunchArgument(
+            "rviz",
+            default_value="true",
+            description="Start RViz2 automatically with this launch file.",
+        )
+    
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value=localisation_params_file,
+        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
+    
+    sim = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    pkg_sim,'launch','launch_sim.launch.py'
+                )]),
+    )
+
+    rviz_config_file = os.path.join(pkg_slam, "rviz", "slam_config.rviz")
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(rviz),
+    )
+
+    localisation = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    pkg_slam,'launch','online_async_launch.py'
+                )]),launch_arguments={'slam_params_file': slam_params_file,}.items()
+    )
+
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_rviz)
+    ld.add_action(declare_params_file_cmd)
+
+    # Add any actions
+    ld.add_action(sim)
+    ld.add_action(rviz_node)
+    ld.add_action(localisation)
+
+    # Launch them all!
+    return ld
