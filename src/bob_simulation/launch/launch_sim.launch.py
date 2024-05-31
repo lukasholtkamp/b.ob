ca4de9525c443a29ccaeab57d451c18e10046b25
jkan67-de <jkan67.de@gmail.com>
2024-05-31 22:27:54 +0200

    edit views

--- src/bob_simulation/launch/launch_sim.launch.py
+++ src/bob_simulation/launch/launch_sim.launch.py
@@ -128,7 +128,7 @@ def generate_launch_description():
     ld.add_action(start_robot_localization_cmd)
     ld.add_action(joy_node)
     ld.add_action(teleop_node)
-    ld.add_action(rviz_node)
+    # ld.add_action(rviz_node)
 
 
     # Launch them all!
