648e765020523e4f5f34979179f9a005e242af58
jkan67-de <jkan67.de@gmail.com>
2024-05-31 12:50:44 +0200

    able to launch sim with obstacle world

--- src/bob_simulation/launch/launch_sim.launch.py
+++ src/bob_simulation/launch/launch_sim.launch.py
@@ -4,8 +4,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
@@ -14,23 +15,50 @@ from pathlib import Path
 
 def generate_launch_description():
 
+    pkg_path = os.path.join(Path.cwd(), "src", "bob_simulation")
+    pkg_teleop = os.path.join(Path.cwd(), "src", "bob_teleop")
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    gazebo_params_file = os.path.join(pkg_path, "config/gazebo_params.yaml")
+    world_filename = "obstacle.world"
+    world_path = os.path.join(pkg_path, "worlds", world_filename)
 
-    package_name='bob_simulation' #<--- CHANGE ME
+    # Launch configuration variables specific to simulation
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_ros2_control = LaunchConfiguration("use_ros2_control")
+    world = LaunchConfiguration("world")
+
+    # Declare the launch arguments
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        name="use_sim_time",
+        default_value="True",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    declare_use_ros2_control_cmd = DeclareLaunchArgument(
+        name="use_ros2_control",
+        default_value="False",
+        description="Use ros2_control if true",
+    )
+
+    declare_world_cmd = DeclareLaunchArgument(
+        name="world",
+        default_value=world_path,
+        description="Full path to the world model to load",
+    )
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                    pkg_path,'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': use_sim_time,'use_ros2_control': use_ros2_control,}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),launch_arguments={
+            "world": world,
+            "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file,}.items(),
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -45,9 +73,7 @@ def generate_launch_description():
         executable="joy_node",
     )
 
-    config_filepath = os.path.join(
-        Path.cwd(), "src", "bob_teleop", "config", "ps2.config.yaml"
-    )
+    config_filepath = os.path.join(pkg_teleop, "config", "ps2.config.yaml")
 
     # Launch drive selection node to be able to switch between the modes
     teleop_node = Node(
@@ -60,12 +86,20 @@ def generate_launch_description():
                 output="screen",
             )
 
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_use_ros2_control_cmd)
+    ld.add_action(declare_world_cmd)
+
+    # Add any actions
+    ld.add_action(rsp)
+    ld.add_action(gazebo)
+    ld.add_action(spawn_entity)
+    ld.add_action(joy_node)
+    ld.add_action(teleop_node)
 
     # Launch them all!
-    return LaunchDescription([
-        rsp,
-        gazebo,
-        spawn_entity,
-        joy_node,
-        teleop_node
-    ])
+    return ld
