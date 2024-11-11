42e221813dd0f17ef64c2a84f0b8ac02e69ea7f3
jkan67-de <jkan67.de@gmail.com>
2024-11-11 10:38:52 +0100

    scan mode changed

--- src/bob_lidar/launch/rplidar.launch.py
+++ src/bob_lidar/launch/rplidar.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
     frame_id = LaunchConfiguration("frame_id", default="lidar_frame")
     inverted = LaunchConfiguration("inverted", default="false")
     angle_compensate = LaunchConfiguration("angle_compensate", default="true")
-    scan_mode = LaunchConfiguration("scan_mode", default="Standard")
+    scan_mode = LaunchConfiguration("scan_mode", default="Stability")
 
     # Get config path of the PID settings
     pid_config_filepath = os.path.join(
